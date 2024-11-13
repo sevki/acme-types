5a5021e74e50d0437090a6f3609b3c26548b50f7
Sevki <s@sevki.io>
2024-11-13 19:05:05 +0000

    import newfiles

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+/// Rust primitives for [RFC 8555](https://datatracker.ietf.org/doc/html/rfc8555) resources
+///
+/// ```
+/// use acme_types::v2 as ACME;
+///
+/// let resp = reqwest::blocking::get("https://acme-v02.api.letsencrypt.org/directory")
+///     .unwrap()
+///     .text()
+///     .unwrap();
+///
+/// let directory = ACME::Directory::from_str(&resp).unwrap();
+///
+/// println!("{:?}", directory);
+///```
+pub mod v2;
